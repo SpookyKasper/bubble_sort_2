a991d91c0b1363d87fb4386e3bf727b8eeb977b5
Kasper <kasperbak89@pm.me>
2022-08-23 14:54:56 +0200

    Optimize implementation

--- bubble_sort_2.rb
+++ bubble_sort_2.rb
@@ -1,21 +1,20 @@
-my_array = [3, 5, 19, 2, 80, 7, 8, 23]
+my_array = [3, 5, 19, 2, 80, 7, 8, 23, 1]
 def bubble_sort(array)
-  is_sorted = false
   sorted_array = []
   keepGoing = true
-  while(keepGoing && !is_sorted)
-    swaped = false
+  while(keepGoing)
+    swaps = 0
     array.each_with_index.reduce() do |memo, (value, index)|
       if value < memo[0]
         array[index - 1] = value
         array[index] = memo[0]
-        swaped = true
+        swaps += 1
       end
       [array[index], 0]
     end
     sorted_array.unshift(array.pop())
-    array.length < 1 ? keepGoing = false : keepGoing
-    !swaped ? sorted_array.unshift(array) && keepGoing = false : keepGoing
+    array.length < 2 ? sorted_array.unshift(array) && keepGoing = false : keepGoing
+    swaps == 0 ? sorted_array.unshift(array) && keepGoing = false : keepGoing
   end
   sorted_array.flatten
 end
